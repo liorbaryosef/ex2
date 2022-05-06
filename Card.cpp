4c96e56c5515eb130915778e3c3a824dd235d2c6
s-griffith <cmcgriffith@gmail.com>
2022-05-06 21:40:21 +0300

    lost track by now

--- Card.cpp
+++ Card.cpp
@@ -2,6 +2,7 @@
 #include "Card.h"
 #include "utilities.h"
 #include <string> //do i actually need this
+#include "Player.h"
 
 Card::Card(CardType type, const CardStats& stats) {
     this->m_effect = type;
@@ -10,7 +11,7 @@ Card::Card(CardType type, const CardStats& stats) {
 
 void Card::applyEncounter(Player& player) const {
     if (this->m_effect == CardType::Battle) {
-        bool win = (player.force >= this->m_stats.force); //how do you access fields outside of your class?
+        bool win = (player.Player::getAttackStrength() >= this->m_stats.force);
         printBattleResult(win);
         if (win) {
             player.levelUp();
@@ -20,8 +21,8 @@ void Card::applyEncounter(Player& player) const {
             player.damage(this->m_stats.hpLossOnDefeat); //check that not negative in function or here?
         }
     }
-    else if (this->m_effect == CardType::Treasure) { //should this just be if?
-        player.addCoins(this->m_stats.loot); //check that should be loot and not something else
+    else if (this->m_effect == CardType::Treasure) {
+        player.addCoins(this->m_stats.loot);
     }
     else {
         if (player.pay(this->m_stats.cost)) {
@@ -38,16 +39,16 @@ void Card::applyEncounter(Player& player) const {
 void Card::printInfo() const {
     switch (this->m_effect) {
         case CardType::Battle: 
-            printBattleCardInfo(this);
+            printBattleCardInfo(this->m_stats);
             break;
         case CardType::Buff:
-            printBuffCardInfo(this);
+            printBuffCardInfo(this->m_stats);
             break;
         case CardType::Heal:
-            printHealCardInfo(this);
+            printHealCardInfo(this->m_stats);
             break;
         case CardType::Treasure:
-            printTreasureCardInfo(this);
+            printTreasureCardInfo(this->m_stats);
             break;
     }
 }
