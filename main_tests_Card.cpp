5ea4a4cba38dd08350f8407f8edd20eaa62b4871
s-griffith <cmcgriffith@gmail.com>
2022-05-06 22:02:10 +0300

    added tests - all work!

--- main_tests_Card.cpp
+++ main_tests_Card.cpp
@@ -4,18 +4,36 @@
 
 int main() {
     CardStats stats(3, 40, 10, 30, 1, 20);
-    Card card(CardType::Battle, stats);
-    card.Card::printInfo();
+    Card battle(CardType::Battle, stats);
+    battle.Card::printInfo();
     Player player1("John", 150, 2);
     Player player2("Sam", 300);
     player2.Player::printInfo();
-    card.applyEncounter(player2);
+    battle.applyEncounter(player2);
     player2.Player::printInfo();
     CardStats stats2(4, 5, 6, 7, 8, 9);
-    Card card2(CardType::Buff, stats2);
-    card2.Card::printInfo();
+    Card buff(CardType::Buff, stats2);
+    buff.Card::printInfo();
+    std::cout << "Applying buff to John" << std::endl;
+    buff.applyEncounter(player1);
     player1.Player::printInfo();
-    card2.applyEncounter(player2);
+    std::cout << "Applying buff to Sam" << std::endl;
+    buff.applyEncounter(player2);
     player2.Player::printInfo();
+    std::cout << "Applying battle to John" << std::endl;
+    battle.applyEncounter(player1);
+    player1.Player::printInfo();
+    buff.applyEncounter(player2);
+    player2.Player::printInfo();
+    CardStats statsStrong(400, 40, 34, 2, 3, 50);
+    Card strongBattle(CardType::Battle, statsStrong);
+    strongBattle.Card::applyEncounter(player1);
+    player1.Player::printInfo();
+    CardStats stats3(1, 2, 3, 4, 5, 6);
+    Card heal(CardType::Heal, stats3);
+    heal.Card::printInfo();
+    heal.applyEncounter(player1);
+    player1.Player::printInfo();
+
     return 0;
 }
