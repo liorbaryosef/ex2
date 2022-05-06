491216160181f3e897afb14c7defb8d0de2ebd3a
liorbaryosef <bar.lior@campus.technion.ac.il>
2022-05-06 17:39:25 +0300

    Player's class and tests

---
+++ Player.cpp
@@ -0,0 +1,157 @@
+//
+// Created by liorb on 06/05/2022.
+//
+
+#include "Player.h"
+#include "utilities.h"
+#include <iostream>
+#include <string>
+using std::cout;
+using std::endl;
+
+
+//--------------------------------Helper Functions--------------------------------
+int validateHP(int givenMaxHP);
+std::string validateName(std::string givenName);
+int validateForce(int givenForce);
+//--------------------------------------------------------------------------------
+
+
+//Constructor for player
+Player::Player(std::string givenName, int givenMaxHP /*= DEFAULT_MAX_HP*/, int givenForce /*= DEFAULT_FORCE*/) \
+    : m_maxHP{validateHP(givenMaxHP)}
+{
+    m_name = validateName(givenName);
+    m_level = 1;
+    m_force = validateForce(givenForce);
+    m_hp = validateHP(givenMaxHP);
+    m_coins = 0;
+
+}
+
+//Prints to screen the player's current stats
+void Player::printInfo() const
+{
+    printPlayerInfo(m_name.c_str(), m_level, m_force, m_hp, m_coins);
+}
+
+//Increases the player's level, until maximum level (10) is reached
+void Player::levelUp()
+{
+    if (m_level < MAX_LEVEL) {
+        m_level++;
+    }
+}
+
+//Returns the player's current level
+int Player::getLevel() const
+{
+    return m_level;
+}
+
+//Increases player's force by requested amount
+void Player::buff(int increaseForce)
+{
+    if (increaseForce >= 0) {
+        m_force += increaseForce;
+    }
+}
+
+//Increases player's Health Points by requested amount, until maximum is reached
+void Player::heal(int increaseHP)
+{
+    if (increaseHP >= 0) {
+        if ((m_hp + increaseHP) > m_maxHP) {
+            m_hp = m_maxHP;
+        }
+        else {
+            m_hp += increaseHP;
+        }
+    }
+}
+
+//Decreases player's Health Points by requested amount, until the value is 0
+void Player::damage(int decreaseHP)
+{
+    if (decreaseHP >= 0) {
+        if ((m_hp - decreaseHP) < 0) {
+            m_hp = 0;
+        }
+        else {
+            m_hp -= decreaseHP;
+        }
+    }
+}
+
+//Checks if the player's Health Points has reached 0
+//Return true if it has, otherwise return false
+bool Player::isKnockedOut() const
+{
+    if (m_hp == 0) {
+        return true;
+    }
+    return false;
+}
+
+//Increases the player's coins by the requested amount
+void Player::addCoins(int increaseCoins)
+{
+    if (increaseCoins >= 0) {
+        m_coins += increaseCoins;
+    }
+}
+
+//Decreases the player's coins by the requested amount.
+//Return true if the payment was completed successfully, otherwise return false.
+bool Player::pay(int decreaseCoins)
+{
+    if ((decreaseCoins >= 0) && (m_coins - decreaseCoins >= 0)) {
+        m_coins -= decreaseCoins;
+        return true;
+    }
+    return false;
+}
+
+//Returns the players attack strength: the player's level + the player's force
+int Player::getAttackStrength() const
+{
+    return (m_level + m_force);
+}
+
+
+//--------------------------------Helper Functions--------------------------------
+
+//Validates the given maximum Health Point value from input
+int validateHP(int givenMaxHP)
+{
+    if (givenMaxHP < 0) {
+        return DEFAULT_MAX_HP;
+    }
+    return givenMaxHP;
+}
+
+
+//Validates the given name from input - only English letters without spaces
+std::string validateName(std::string givenName)
+{
+    if (givenName.empty()) {
+        return DEFAULT_NAME;
+    }
+    for (std::string::size_type i = 0; i < givenName.size(); i++) {
+        if ((givenName[i] < 'A') || (givenName[i] > 'Z' && givenName[i] < 'a') \
+            || (givenName[i] > 'z')) {
+            return DEFAULT_NAME;
+        }
+    }
+    return givenName;
+}
+
+
+//Validates the force from input
+int validateForce(int givenForce)
+{
+    if (givenForce < 0) {
+        return DEFAULT_FORCE;
+    }
+    return givenForce;
+}
---
+++ Player.h
@@ -0,0 +1,144 @@
+//
+// Created by liorb on 06/05/2022.
+//
+
+#ifndef EX2_PLAYER_H
+#define EX2_PLAYER_H
+
+#include <string>
+#include "utilities.h"
+
+#define DEFAULT_MAX_HP 100
+#define DEFAULT_FORCE 5
+#define DEFAULT_NAME "defaultPlayer"
+#define MAX_LEVEL 10
+
+/*
+ *  Player:
+ *  This class represents a single player in the game.
+*/
+
+class Player {
+public:
+    /*
+     * C'tor of Player class
+     *
+     * @param name - The name of the player
+     * @param HP - Starting amount of health points, between [0, maxHP], default is 100
+     * @param force - Starting force, default is 5
+     * @return
+     *      A new instance of Player.
+    */
+    Player(std::string givenName, int givenMaxHP = DEFAULT_MAX_HP, int givenForce = DEFAULT_FORCE);
+
+
+    /*
+     * Prints the Player info
+     *
+     * @return
+     *      void
+    */
+    void printInfo() const;
+
+
+    /*
+     * Increasing the player's level by one
+     *
+     * @return
+     *      void
+    */
+    void levelUp();
+
+
+    /*
+     * Return the player's current level
+     *
+     * @return
+     *      int - the current level
+    */
+    int getLevel() const;
+
+
+    /*
+     * Increases the player's force by the given amount
+     *
+     * @return
+     *      void
+    */
+    void buff(int increaseForce);
+
+
+    /*
+     * Increases the player's health points by the given amount, until maxHP reached
+     *
+     * @return
+     *      void
+    */
+    void heal(int increaseHP);
+
+
+    /*
+     * Decrease the player's health points by the given amount, until the value 0
+     *
+     * @return
+     *      void
+    */
+    void damage(int decreaseHP);
+
+
+    /*
+     * Checks if the players health points has reached 0
+     *
+     * @return
+     *      boolean value - return true if the HP is 0, otherwise return false
+    */
+    bool isKnockedOut() const;
+
+
+    /*
+     * Adds the given amount of coins to the player's purse
+     *
+     * @return
+     *      void
+    */
+    void addCoins(int increaseCoins);
+
+
+    /*
+     * Removes the requested amount of coins from the player's purse
+     *
+     * @return
+     *      boolean value - return true if the payment was completed successfully, otherwise return false
+    */
+    bool pay(int decreaseCoins);
+
+
+    /*
+     * Return the attack strength = player's force + player's level
+     *
+     * @return
+     *      the calculated attach strength
+    */
+    int getAttackStrength() const;
+
+
+    /*
+     * Here we are explicitly telling the compiler to use the default methods
+     *      for the copy constructor, destructor and the override of the "=" operator
+    */
+    Player(const Player&) = default;
+    ~Player() = default;
+    Player& operator=(const Player& other) = default;
+
+
+private:
+    std::string m_name;
+    int m_level;
+    int m_force;
+    int m_hp;
+    int m_coins;
+    const int m_maxHP;
+
+};
+
+#endif //EX2_PLAYER_H
