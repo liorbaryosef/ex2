a52095d60e9299d8756cd8a57ebfa8386865a91d
s-griffith <cmcgriffith@gmail.com>
2022-05-07 19:32:55 +0300

    got rid of this pointer

--- Card.cpp
+++ Card.cpp
@@ -1,54 +1,53 @@
 #include <iostream>
 #include "Card.h"
 #include "utilities.h"
-#include <string> //do i actually need this
 #include "Player.h"
 
 Card::Card(CardType type, const CardStats& stats) {
-    this->m_effect = type;
-    this->m_stats = stats;
+    m_effect = type;
+    m_stats = stats;
 }
 
-void Card::applyEncounter(Player& player) const {
-    if (this->m_effect == CardType::Battle) {
-        bool win = (player.Player::getAttackStrength() >= this->m_stats.force);
+void Card::applyEncounter(Player& player) const { //check if player is not null???
+    if (m_effect == CardType::Battle) {
+        bool win = (player.getAttackStrength() >= m_stats.force);
         printBattleResult(win);
         if (win) {
             player.levelUp();
-            player.addCoins(this->m_stats.loot);
+            player.addCoins(m_stats.loot);
         }
         else {
-            player.damage(this->m_stats.hpLossOnDefeat); //check that not negative in function or here?
+            player.damage(m_stats.hpLossOnDefeat);
         }
     }
-    else if (this->m_effect == CardType::Treasure) {
-        player.addCoins(this->m_stats.loot);
+    else if (m_effect == CardType::Treasure) {
+        player.addCoins(m_stats.loot);
     }
     else {
-        if (player.pay(this->m_stats.cost)) {
-            if (this->m_effect == CardType::Heal) {
-                player.heal(this->m_stats.heal);
+        if (player.pay(m_stats.cost)) {
+            if (m_effect == CardType::Heal) {
+                player.heal(m_stats.heal);
             }
             else{
-                player.buff(this->m_stats.buff);
+                player.buff(m_stats.buff);
             }
         }
     }
 }
 
 void Card::printInfo() const {
-    switch (this->m_effect) {
+    switch (m_effect) {
         case CardType::Battle: 
-            printBattleCardInfo(this->m_stats);
+            printBattleCardInfo(m_stats);
             break;
         case CardType::Buff:
-            printBuffCardInfo(this->m_stats);
+            printBuffCardInfo(m_stats);
             break;
         case CardType::Heal:
-            printHealCardInfo(this->m_stats);
+            printHealCardInfo(m_stats);
             break;
         case CardType::Treasure:
-            printTreasureCardInfo(this->m_stats);
+            printTreasureCardInfo(m_stats);
             break;
     }
 }
